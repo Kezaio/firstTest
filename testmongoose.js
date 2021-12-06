537cb87f0efd7e3ef3e045b47b270f3372cead90
likeke <2422014722@qq.com>
2021-12-05 17:06:49 -0800

    secondcommit

--- testmongoose.js
+++ testmongoose.js
@@ -10,6 +10,8 @@ const mydata = mongoose.model('cats', schema);
 // const kitty = new mydata({ name: '001' });
 // kitty.save().then(() => console.log('meow'));
 
+// const Hellokitty = new mydata({ name: '002',age:1,health:"good" });
+// Hellokitty.save().then(() => console.log('aow'));
 mydata.find({name:"yingtao"},(err,data)=>{
     console.log(data[0]._doc)
 })
